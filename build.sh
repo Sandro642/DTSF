3226d3e3649504c1452f0d64f40a0c9ff36cbb71
Sandro Soria <sandro33810@gmail.com>
2023-12-23 21:30:11 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Clonage du repository GitHub
+git clone https://github.com/Sandro642/DTSF.git
+
+# Accéder au répertoire cloné
+cd DTSF
+
+# Installation des dépendances (remplacer cette commande par celle nécessaire à votre projet)
+# Exemple avec npm pour un projet Node.js
+npm install
+
+# Revenir au répertoire parent
+cd ..
+
+# Suppression du script .sh
+rm script.sh
