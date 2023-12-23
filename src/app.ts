8518829a892db49363882edfe6d6202fc270a353
Sandro642 <sandro33810@gmail.com>
2023-12-23 21:44:13 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,15 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+
 app.get('/', (req, res) => {
   res.send('Serveur en ligne !');
 });
-app.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on ${port}`);});
+
+const server = app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
+
+server.on('error', (err) => {
+  console.error('Error starting server:', err);
+});
